c8e6d70490605720ef31b5f0453e067f33cebd99
Niles O'Neil <nilesoneil@gmail.com>
2022-04-07 19:24:34 -0700

    added hidden elememts to html using css

--- js/scripts.js
+++ js/scripts.js
@@ -7,42 +7,51 @@ $(document).ready(function() {
     const pet = $("input:radio[name=animal]:checked").val()
 
     if (interests === "puzzles" && weather === "Sunny" && pet === "snake") {
+      $(".hidden").toggle();
       $("#ruby").hide();
       $("#javascript").show();
       $("#swift").hide();
     } else if (interests === "puzzles" && weather === "Rainy" && pet === "snake") {
+      $(".hidden").toggle();
       $("#ruby").hide();
       $("#javascript").show();
       $("#swift").hide();
     } else if (interests === "puzzles" && weather === "Overcast" && pet === "snake") {
+      $(".hidden").toggle();
       $("#ruby").hide();
       $("#javascript").show();
       $("#swift").hide();
     } else if (interests === "running" && weather === "Overcast" && pet === "dog") {
+      $(".hidden").toggle();
       $("#ruby").hide();
       $("#javascript").hide();
       $("#swift").show();
     } else if (interests === "running" && weather === "Sunny" && pet === "dog") {
+      $(".hidden").toggle();
       $("#ruby").hide();
       $("#javascript").hide();
       $("#swift").show();
     } else if (interests === "running" && weather === "Rainy" && pet === "dog") {
+      $(".hidden").toggle();
       $("#ruby").hide();
       $("#javascript").hide();
       $("#swift").show();
     } else if (interests === "mining" && weather === "Rainy" && pet === "cat") {
+      $(".hidden").toggle();
       $("#ruby").show();
       $("#javascript").hide();
       $("#swift").hide();
     } else if (interests === "mining" && weather === "Sunny" && pet === "cat") {
+      $(".hidden").toggle();
       $("#ruby").show();
       $("#javascript").hide();
       $("#swift").hide();
-    } else
+    } else {
+      $(".hidden").toggle();
       $("#swift").show();
       $("#javascript").hide();
       $("#ruby").hide();
-
-  event.preventDefault();
+    }
+    event.preventDefault();
   });
 });
