269ff59a19faf850984cc4a2e7decd849e3ff07e
Niles O'Neil <nilesoneil@gmail.com>
2022-04-07 18:49:54 -0700

    add more ui logic for what language is best for user

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,43 @@ $(document).ready(function() {
     const weather = $("#weather").val();
     const pet = $("input:radio[name=animal]:checked").val()
 
+    if (interests === "puzzles" && weather === "Sunny" && pet === "snake") {
+      $("#ruby").hide();
+      $("#javascript").show();
+      $("#swift").hide();
+    } else if (interests === "puzzles" && weather === "Rainy" && pet === "snake") {
+      $("#ruby").hide();
+      $("#javascript").show();
+      $("#swift").hide();
+    } else if (interests === "puzzles" && weather === "Overcast" && pet === "snake") {
+      $("#ruby").hide();
+      $("#javascript").show();
+      $("#swift").hide();
+    } else if (interests === "running" && weather === "Overcast" && pet === "dog") {
+      $("#ruby").hide();
+      $("#javascript").hide();
+      $("#swift").show();
+    } else if (interests === "running" && weather === "Sunny" && pet === "dog") {
+      $("#ruby").hide();
+      $("#javascript").hide();
+      $("#swift").show();
+    } else if (interests === "running" && weather === "Rainy" && pet === "dog") {
+      $("#ruby").hide();
+      $("#javascript").hide();
+      $("#swift").show();
+    } else if (interests === "mining" && weather === "Rainy" && pet === "cat") {
+      $("#ruby").show();
+      $("#javascript").hide();
+      $("#swift").hide();
+    } else if (interests === "mining" && weather === "Sunny" && pet === "cat") {
+      $("#ruby").show();
+      $("#javascript").hide();
+      $("#swift").hide();
+    } else
+      $("#swift").show();
+      $("#javascript").hide();
+      $("#ruby").hide();
+
   event.preventDefault();
   });
 });
