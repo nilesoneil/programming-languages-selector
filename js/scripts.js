0abc8b74da046343ad1c9e65cf6a6c64dc4af499
Niles O'Neil <nilesoneil@gmail.com>
2022-04-07 18:03:48 -0700

    set up basic UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-  $("form#pastry").submit(function(event) {
+  $("#surveyForm").submit(function(event) {
+    const name = $("#userName").val();
+    const dob = $("#born").val();
+    const interests = $("#interests").val();
+    const weather = $("#weather").val();
+    const pet = $("input:radio[name=animal]:checked").val()
 
-    event.preventDefault();
+  event.preventDefault();
   });
 });
